99dfed6bdc126085286f9cb740d1c0e7d1dc8cac
showrab09 <showrabbinhabib@gmail.com>
2021-08-17 01:17:48 +0600

    All Complete

---
+++ bank.js
@@ -0,0 +1,14 @@
+document.getElementById("login-submit").addEventListener("click", function() {
+    const emailField = document.getElementById('user-email');
+    const email = emailField.value;
+
+    const passField = document.getElementById('user-password');
+    const password = passField.value;
+    if (email == "abc@gmail.com" && password == "123456") {
+       window.location.href='banking.html'
+    }
+    else {
+        alert('Please Enter Valid Email and Password');
+    }
+});
+
